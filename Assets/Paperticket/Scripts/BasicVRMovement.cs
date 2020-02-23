4b76ecabe8821faecca0225c51e7876e51a17137
Sean O'Connell <pinky@paperticketstudios.com>
2020-02-24 05:02:00 +1100

    Upgraded to 2019.3.2f1 and added second greybox

---
+++ Assets/Paperticket/Scripts/BasicVRMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+namespace Paperticket
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class BasicVRMovement : MonoBehaviour
+    {
+
+
+
+        [SerializeField] CapsuleCollider capsuleController;
+
+        [SerializeField] float moveSpeed;
+
+        Rigidbody rb;
+
+        Vector2 input;
+
+        private void Awake() {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate() {
+            float speed = GetInput();
+
+            // always move along the camera forward as it is the direction that it being aimed at
+            Vector3 desiredMove = transform.forward * input.y + transform.right * input.x;
+
+            // get a normal for the surface that is being touched to move along it
+            RaycastHit hitInfo;
+            Physics.SphereCast(transform.position, capsuleController.radius, Vector3.down, out hitInfo,
+                               capsuleController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+            desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+
+            Vector3 newPosition = rb.position + (speed * new Vector3(desiredMove.x, 0, desiredMove.z));
+
+            rb.MovePosition(newPosition);
+            
+        }
+
+
+
+        float GetInput() {
+
+            // Read input
+            float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+            float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+            input = new Vector2(horizontal, vertical);
+
+            // normalize input if it exceeds 1 in combined length:
+            if (input.sqrMagnitude > 1) {
+                input.Normalize();
+            }
+
+            return moveSpeed;
+
+        }
+
+
+
+
+
+    }
+
+
+}
