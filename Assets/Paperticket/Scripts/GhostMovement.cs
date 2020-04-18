18721557edb0a8b56321282855de69997c7b84c5
Sean O'Connell <pinky@paperticketstudios.com>
2020-04-18 21:57:20 +1000

    ATTN: Fixing critical build errors
    please download this version before making anymore changes

--- Assets/Paperticket/Scripts/GhostMovement.cs
+++ Assets/Paperticket/Scripts/GhostMovement.cs
@@ -1,222 +1,221 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class GhostMovement : MonoBehaviour
-{
-
-    [Header("References")]
-
-    [SerializeField] private Transform WaypointsParent;
-    private List<Transform> waypoints = new List<Transform>();
-
-    [Header("Controls")]
-
-    [SerializeField] private float minWaitDuration;
-    [SerializeField] private float maxWaitDuration;
-    [SerializeField] private int checkFrequency;        // How long to wait between checks to see if we've reached our destination
-    [SerializeField] private float pauseAfterTurning;   // How long to wait after turning towards a new wandering path
-    [SerializeField] private bool debugging;
-
-    [Header("Read Only")]
-
-    [SerializeField] private Vector3 currentTarget;
-    [SerializeField] private float smoothing;
-
-    private int previousIndex;
-    private NavMeshAgent agent;
-
-
-    // Start is called before the first frame update
-    void Start()
+namespace Paperticket {
+    public class GhostMovement : MonoBehaviour
     {
-        // Grab the nav agent reference
-        agent = agent ?? GetComponent<NavMeshAgent>();
-        if (!agent) {
-            Debug.LogError("[GhostMovement] ERROR -> No NavMeshAgent found on this object! Please add one. Disabling object.");
-            gameObject.SetActive(false);
+
+        [Header("References")]
+
+        [SerializeField] private Transform WaypointsParent;
+        private List<Transform> waypoints = new List<Transform>();
+
+        [Header("Controls")]
+
+        [SerializeField] private float minWaitDuration;
+        [SerializeField] private float maxWaitDuration;
+        [SerializeField] private int checkFrequency;        // How long to wait between checks to see if we've reached our destination
+        [SerializeField] private float pauseAfterTurning;   // How long to wait after turning towards a new wandering path
+        [SerializeField] private bool debugging;
+
+        [Header("Read Only")]
+
+        [SerializeField] private Vector3 currentTarget;
+        [SerializeField] private float smoothing;
+
+        private int previousIndex;
+        private NavMeshAgent agent;
+
+
+        // Start is called before the first frame update
+        void Start() {
+            // Grab the nav agent reference
+            agent = agent ?? GetComponent<NavMeshAgent>();
+            if (!agent) {
+                Debug.LogError("[GhostMovement] ERROR -> No NavMeshAgent found on this object! Please add one. Disabling object.");
+                gameObject.SetActive(false);
+            }
+
+            // Grab the WaypointsParent reference
+            WaypointsParent = WaypointsParent ?? GameObject.Find("Waypoints").transform;
+            if (!WaypointsParent) {
+                Debug.LogError("[GhostMovement] ERROR -> No WaypointsParent could be found! Please add one to the scene. Disabling object.");
+                gameObject.SetActive(false);
+            }
+
+            // Grab the waypoints reference
+
+            foreach (Transform t in WaypointsParent.GetComponentsInChildren<Transform>()) if (t != WaypointsParent) waypoints.Add(t);
+            if (waypoints.Count == 0) {
+                Debug.LogError("[GhostMovement] ERROR -> No waypoints found under WaypointsParent! Please add children to WaypointsParent. Disabling object.");
+                gameObject.SetActive(false);
+            }
+
+
+
+            //agent.updateRotation = false;
+
+
+
+            StartCoroutine(Waiting());
         }
 
-        // Grab the WaypointsParent reference
-        WaypointsParent = WaypointsParent ?? GameObject.Find("Waypoints").transform;
-        if (!WaypointsParent) {
-            Debug.LogError("[GhostMovement] ERROR -> No WaypointsParent could be found! Please add one to the scene. Disabling object.");
-            gameObject.SetActive(false);
+
+        // Use this for checking to see player later
+        //void Update()
+        //{
+        //    //
+        //}
+
+
+
+        public void PickNewWaypoint() {
+
+            if (debugging) Debug.Log("[GhostMovement] Picking new waypoint..");
+
+
+            // Pick a new waypoint, must be different from the previous one
+            int newIndex = previousIndex;
+            while (newIndex == previousIndex) {
+                newIndex = Random.Range(0, waypoints.Count - 1);
+            }
+            previousIndex = newIndex;
+
+            if (debugging) Debug.Log("[GhostMovement] Picked " + waypoints[newIndex] + "!");
+
+            //Turn off the navmesh agents own rotation
+            //agent.updatePosition = false;
+            //agent.updateRotation = false;
+
+            // Set the waypoint as the next destination
+            currentTarget = waypoints[newIndex].position;
+            agent.SetDestination(currentTarget);
+
+            // Rotate towards the new destination
+            StartCoroutine(TurningToTargetDirection());
+
+
         }
 
-        // Grab the waypoints reference
-        
-        foreach (Transform t in WaypointsParent.GetComponentsInChildren<Transform>()) if (t != WaypointsParent) waypoints.Add(t);
-        if (waypoints.Count == 0) {
-            Debug.LogError("[GhostMovement] ERROR -> No waypoints found under WaypointsParent! Please add children to WaypointsParent. Disabling object.");
-            gameObject.SetActive(false);
-        }
-
-
-
-        //agent.updateRotation = false;
-
-
-
-        StartCoroutine(Waiting());      
-    }
-
-
-    // Use this for checking to see player later
-    //void Update()
-    //{
-    //    //
-    //}
-
-
-
-    public void PickNewWaypoint() {
-
-        if (debugging) Debug.Log("[GhostMovement] Picking new waypoint..");
-
-
-        // Pick a new waypoint, must be different from the previous one
-        int newIndex = previousIndex;
-        while (newIndex == previousIndex) {
-            newIndex = Random.Range(0, waypoints.Count - 1);
-        }
-        previousIndex = newIndex;
-
-        if (debugging) Debug.Log("[GhostMovement] Picked " + waypoints[newIndex] + "!");
-
-        //Turn off the navmesh agents own rotation
-        //agent.updatePosition = false;
-        //agent.updateRotation = false;
-
-        // Set the waypoint as the next destination
-        currentTarget = waypoints[newIndex].position;
-        agent.SetDestination(currentTarget);
-        
-        // Rotate towards the new destination
-        StartCoroutine(TurningToTargetDirection());
-
-
-    }
-
-    IEnumerator TurningToTargetDirection() {
-
-        yield return null;
-        agent.isStopped = true;
-
-        if (debugging) Debug.Log("[GhostMovement] Turning to target direction..");
-
-        // Get the rotation to face the target direction        
-        while (!agent.hasPath) {
-            Debug.Log("[GhostMovement] (waiting for path)");
-            yield return null;
-        }
-        Quaternion lookRotation = Quaternion.LookRotation((agent.path.corners[1] - transform.position).normalized, Vector3.up);
-        //agent.steeringTarget
-        //agent.velocity
-        //agent.path.corners[1]
-
-        if (debugging) Debug.Log("[GhostMovement] Look rotation = " + lookRotation.eulerAngles);
-
-        //transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, agent.angularSpeed * Time.deltaTime * 0.05f);
-                
-        float initialAngle = Quaternion.Angle(transform.rotation, lookRotation);
-        // Continue once the angle to the target direction is acceptable
-        while (Quaternion.Angle(transform.rotation, lookRotation) > 0.5 ) {
-
-            if (debugging) Debug.Log("[GhostMovement] Initial angle = " + initialAngle);
-            if (debugging) Debug.Log("[GhostMovement] Current angle = " + Quaternion.Angle(transform.rotation, lookRotation));
-
-            // t = 0 -> 1
-            // smoothing = Mathf.SmoothStep(minDelta, maxDelta, currentAngle / initialAngle)
-            // rotatetowards (current, target, smoothing * time.deltaTime)
-
-
-            //float currentAngle = Quaternion.Angle(transform.rotation, lookRotation);
-            //smoothing = Mathf.SmoothStep(30, 60, (1 - currentAngle / initialAngle));
-
-            smoothing = agent.angularSpeed;
-
-            //if (debugging) Debug.Log("[GhostMovement] 1 - CA / IA = " + (1 - currentAngle / initialAngle));
-
-            // Rotate towards the target direction
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, smoothing * Time.deltaTime);
+        IEnumerator TurningToTargetDirection() {
 
             yield return null;
+            agent.isStopped = true;
+
+            if (debugging) Debug.Log("[GhostMovement] Turning to target direction..");
+
+            // Get the rotation to face the target direction        
+            while (!agent.hasPath) {
+                Debug.Log("[GhostMovement] (waiting for path)");
+                yield return null;
+            }
+            Quaternion lookRotation = Quaternion.LookRotation((agent.path.corners[1] - transform.position).normalized, Vector3.up);
+            //agent.steeringTarget
+            //agent.velocity
+            //agent.path.corners[1]
+
+            if (debugging) Debug.Log("[GhostMovement] Look rotation = " + lookRotation.eulerAngles);
+
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, agent.angularSpeed * Time.deltaTime * 0.05f);
+
+            float initialAngle = Quaternion.Angle(transform.rotation, lookRotation);
+            // Continue once the angle to the target direction is acceptable
+            while (Quaternion.Angle(transform.rotation, lookRotation) > 0.5) {
+
+                if (debugging) Debug.Log("[GhostMovement] Initial angle = " + initialAngle);
+                if (debugging) Debug.Log("[GhostMovement] Current angle = " + Quaternion.Angle(transform.rotation, lookRotation));
+
+                // t = 0 -> 1
+                // smoothing = Mathf.SmoothStep(minDelta, maxDelta, currentAngle / initialAngle)
+                // rotatetowards (current, target, smoothing * time.deltaTime)
+
+
+                //float currentAngle = Quaternion.Angle(transform.rotation, lookRotation);
+                //smoothing = Mathf.SmoothStep(30, 60, (1 - currentAngle / initialAngle));
+
+                smoothing = agent.angularSpeed;
+
+                //if (debugging) Debug.Log("[GhostMovement] 1 - CA / IA = " + (1 - currentAngle / initialAngle));
+
+                // Rotate towards the target direction
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, smoothing * Time.deltaTime);
+
+                yield return null;
+            }
+
+
+
+
+
+            // Brief pause before start moving
+            yield return new WaitForSeconds(pauseAfterTurning);
+
+            if (debugging) Debug.Log("[GhostMovement] Rotated successfully!");
+
+            agent.isStopped = false;
+
+            // Start wandering towards the new target
+            StartCoroutine(Wandering());
+
+        }
+
+
+        IEnumerator Wandering() {
+
+            if (debugging) Debug.Log("[GhostMovement] Wandering..");
+
+            //agent.updatePosition = true;
+            //agent.updateRotation = true;
+
+            // DESTINATION CHECK
+            bool arrived = false;
+            while (!arrived) {
+
+                // Determine if we are still attempting to calculate a path
+                if (!agent.pathPending) {
+
+                    // Are we close enough to the minimum stopping distance?
+                    if (agent.remainingDistance <= agent.stoppingDistance) {
+
+                        // Either there's no path left to take, or our speed is close enough to 0 
+                        if (!agent.hasPath || Mathf.Approximately(agent.velocity.sqrMagnitude, 0f)) {
+                            if (debugging) Debug.Log("[GhostMovement] Success!");
+                            arrived = true;
+
+
+                        } else if (debugging) Debug.Log("[GhostMovement] Agent has path, or velocity is still too high..");
+                    } else if (debugging) Debug.Log("[GhostMovement] Remaining distance greater than stopping distance..");
+                } else if (debugging) Debug.Log("[GhostMovement] Path is pending..");
+
+
+                // Wait a while before checking again
+                yield return new WaitForSeconds(checkFrequency == 0 ? Time.deltaTime : Mathf.Abs(checkFrequency));
+            }
+
+            if (debugging) Debug.Log("[GhostMovement] Wandered successfully!");
+
+            StartCoroutine(Waiting());
+
+        }
+
+        IEnumerator Waiting() {
+
+            if (debugging) Debug.Log("[GhostMovement] Waiting..)");
+
+            // Wait a random amount of time
+            yield return new WaitForSeconds(Random.Range(minWaitDuration, maxWaitDuration));
+
+            if (debugging) Debug.Log("[GhostMovement] Waited successfully!");
+
+            PickNewWaypoint();
+
         }
 
 
 
-
-
-        // Brief pause before start moving
-        yield return new WaitForSeconds(pauseAfterTurning);
-
-        if (debugging) Debug.Log("[GhostMovement] Rotated successfully!");
-
-        agent.isStopped = false;
-
-        // Start wandering towards the new target
-        StartCoroutine(Wandering());
-
     }
-
-
-    IEnumerator Wandering() {
-
-        if (debugging) Debug.Log("[GhostMovement] Wandering..");
-
-        //agent.updatePosition = true;
-        //agent.updateRotation = true;
-
-        // DESTINATION CHECK
-        bool arrived = false;
-        while (!arrived) {
-
-            // Determine if we are still attempting to calculate a path
-            if (!agent.pathPending) {
-
-                // Are we close enough to the minimum stopping distance?
-                if (agent.remainingDistance <= agent.stoppingDistance) {                    
-
-                    // Either there's no path left to take, or our speed is close enough to 0 
-                    if (!agent.hasPath || Mathf.Approximately(agent.velocity.sqrMagnitude, 0f)) {
-                        if (debugging) Debug.Log("[GhostMovement] Success!");
-                        arrived = true;
-
-
-                    } else if (debugging) Debug.Log("[GhostMovement] Agent has path, or velocity is still too high..");
-                } else if (debugging) Debug.Log("[GhostMovement] Remaining distance greater than stopping distance..");
-            } else if (debugging) Debug.Log("[GhostMovement] Path is pending..");
-
-
-            // Wait a while before checking again
-            yield return new WaitForSeconds(checkFrequency == 0 ? Time.deltaTime : Mathf.Abs(checkFrequency));
-        }
-
-        if (debugging) Debug.Log("[GhostMovement] Wandered successfully!");
-
-        StartCoroutine(Waiting());
-
-    }
-          
-    IEnumerator Waiting() {
-
-        if (debugging) Debug.Log("[GhostMovement] Waiting..)");
-
-        // Wait a random amount of time
-        yield return new WaitForSeconds(Random.Range(minWaitDuration, maxWaitDuration));
-
-        if (debugging) Debug.Log("[GhostMovement] Waited successfully!");
-        
-        PickNewWaypoint();        
-
-    }
-
-
-
-
 
 
 
--- Assets/Paperticket/Scripts/GhostTransparency.cs
+++ Assets/Paperticket/Scripts/GhostTransparency.cs
@@ -2,108 +2,110 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GhostTransparency : MonoBehaviour
-{
+namespace Paperticket {
+    public class GhostTransparency : MonoBehaviour
+    {
 
-    public SkinnedMeshRenderer ghostMesh;
-    public List<SkinnedMeshRenderer> chainMeshes = new List<SkinnedMeshRenderer>();
+        public SkinnedMeshRenderer ghostMesh;
+        public List<SkinnedMeshRenderer> chainMeshes = new List<SkinnedMeshRenderer>();
 
-    [SerializeField] Material ghostMaterial;
-    [SerializeField] List<Material> chainMaterials = new List<Material>();
+        [SerializeField] Material ghostMaterial;
+        [SerializeField] List<Material> chainMaterials = new List<Material>();
 
-    [Header("Controls")]
-    [SerializeField] float fadeDuration;
+        [Header("Controls")]
+        [SerializeField] float fadeDuration;
 
-    Color ghostMatColor;
-    Color ghostMatTransparent;
-    Color chainMatColor;
-    Color chainMatTransparent;
+        Color ghostMatColor;
+        Color ghostMatTransparent;
+        Color chainMatColor;
+        Color chainMatTransparent;
 
-    void Awake() {
+        void Awake() {
 
-        ghostMaterial = ghostMesh.material;
-        for (int i = 0; i < chainMeshes.Count; i++) {
-            chainMaterials.Add(chainMeshes[i].material);
-        }
-
-        ghostMatColor = ghostMaterial.color;
-        ghostMatTransparent = new Color(ghostMatColor.r, ghostMatColor.g, ghostMatColor.b, 0.1f);
-
-        chainMatColor = chainMaterials[0].color;
-        chainMatTransparent = new Color(chainMatColor.r, chainMatColor.g, chainMatColor.b, 0.1f);
-
-        StartCoroutine(TestingTransparency());
-               
-    }
-
-    IEnumerator TestingTransparency() {
-        while (true) {
-
-            yield return new WaitForSeconds(Random.Range(10f, 18f));
-
-            ToggleGhostTransparency(true);
-            ToggleChainTransparency(true);
-            //if (Random.Range(0f, 10f) > 0.5f) {
-            //    ToggleChainTransparency(true);
-            //}
-
-            yield return new WaitForSeconds(Random.Range(5f, 16f));
-
-            ToggleGhostTransparency(false);
-            ToggleChainTransparency(false);
-            //if (chainMaterials[0].color == chainMatTransparent) {
-            //    ToggleChainTransparency(false);
-            //}
-
-        }
-    }
-
-
-
-
-
-
-    public void ToggleGhostTransparency(bool transparent) {
-        StartCoroutine(FadingColor(new List<Material> { ghostMaterial }, transparent ? ghostMatTransparent : ghostMatColor, fadeDuration));
-    }
-
-    public void ToggleChainTransparency( bool transparent ) {
-        StartCoroutine(FadingColor(chainMaterials, transparent ? chainMatTransparent : chainMatColor, fadeDuration));
-    }
-
-
-    IEnumerator FadingColor(List<Material> materials, Color color, float fadeTime) {
-
-        Color initialColor = materials[0].GetColor("_Color");
-        
-        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / fadeTime) {
-            foreach (Material mat in materials) {
-                Color newColor = Color.Lerp(initialColor, color, t);
-                mat.color = newColor;
-                //mat.SetColor("_Color", newColor);
+            ghostMaterial = ghostMesh.material;
+            for (int i = 0; i < chainMeshes.Count; i++) {
+                chainMaterials.Add(chainMeshes[i].material);
             }
-            yield return null;
+
+            ghostMatColor = ghostMaterial.color;
+            ghostMatTransparent = new Color(ghostMatColor.r, ghostMatColor.g, ghostMatColor.b, 0.1f);
+
+            chainMatColor = chainMaterials[0].color;
+            chainMatTransparent = new Color(chainMatColor.r, chainMatColor.g, chainMatColor.b, 0.1f);
+
+            StartCoroutine(TestingTransparency());
+
         }
 
-        foreach (Material mat in materials) {
-            //mat.SetColor("_Color", color);
-            mat.color = color;
-        }      
+        IEnumerator TestingTransparency() {
+            while (true) {
+
+                yield return new WaitForSeconds(Random.Range(10f, 18f));
+
+                ToggleGhostTransparency(true);
+                ToggleChainTransparency(true);
+                //if (Random.Range(0f, 10f) > 0.5f) {
+                //    ToggleChainTransparency(true);
+                //}
+
+                yield return new WaitForSeconds(Random.Range(5f, 16f));
+
+                ToggleGhostTransparency(false);
+                ToggleChainTransparency(false);
+                //if (chainMaterials[0].color == chainMatTransparent) {
+                //    ToggleChainTransparency(false);
+                //}
+
+            }
+        }
+
+
+
+
+
+
+        public void ToggleGhostTransparency( bool transparent ) {
+            StartCoroutine(FadingColor(new List<Material> { ghostMaterial }, transparent ? ghostMatTransparent : ghostMatColor, fadeDuration));
+        }
+
+        public void ToggleChainTransparency( bool transparent ) {
+            StartCoroutine(FadingColor(chainMaterials, transparent ? chainMatTransparent : chainMatColor, fadeDuration));
+        }
+
+
+        IEnumerator FadingColor( List<Material> materials, Color color, float fadeTime ) {
+
+            Color initialColor = materials[0].GetColor("_Color");
+
+            for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / fadeTime) {
+                foreach (Material mat in materials) {
+                    Color newColor = Color.Lerp(initialColor, color, t);
+                    mat.color = newColor;
+                    //mat.SetColor("_Color", newColor);
+                }
+                yield return null;
+            }
+
+            foreach (Material mat in materials) {
+                //mat.SetColor("_Color", color);
+                mat.color = color;
+            }
+
+        }
+
+
+
+
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
 
     }
 
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
